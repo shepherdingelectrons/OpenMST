3752e3deeab279648144f74be87de9b3c695c042
shepherdingelectrons <shepherdingelectrons@users.noreply.github.com>
2020-04-24 08:49:30 +0100

    Add files via upload

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import OpenMST.MSTProcess as MST
 
 print("Opening file")
-MOCfile = MST.openMOCFile('Your filename here.moc')
+MOCfile = MST.openMOCFile('Your filename here.MOC')
 
 print("Saving experiments to XLSX files")
 MOCfile.SaveXLSX() 
